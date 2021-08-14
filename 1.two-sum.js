64ab6cc7e537fce441af55a5973056a6e27ae04d
KAO-TZU-WEI <sunvicky11@gmail.com>
2021-08-14 13:33:25 +0800

    add a file

---
+++ 1.two-sum.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+let array = [];
+let twoSum = function (nums, target) {
+  let array = [];
+  for (i = 0; i < nums.length; i++) {
+    for (j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        array.push(i, j);
+      }
+    }
+  }
+  return array;
+};
+// @lc code=end
