26cf3beb5dfaf248bbc8dacb28e048564151f4f2
KAO-TZU-WEI <sunvicky11@gmail.com>
2021-08-16 20:59:10 +0800

    changed to O(n)

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -10,16 +10,30 @@
  * @param {number} target
  * @return {number[]}
  */
-let array = [];
+//O(n²)重層回圈
+// let array = [];
+// let twoSum = function (nums, target) {
+//   let array = [];
+//   for (i = 0; i < nums.length; i++) {
+//     for (j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] == target) {
+//         array.push(i, j);
+//       }
+//     }
+//   }
+//   return array;
+// };
+//O(n)以空間換取時間
 let twoSum = function (nums, target) {
-  let array = [];
-  for (i = 0; i < nums.length; i++) {
-    for (j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] == target) {
-        array.push(i, j);
-      }
+  let hash_table = {};
+  let res;
+  for (let i = 0; i < nums.length; i++) {
+    res = target - [nums[i]];
+    if (hash_table[res] !== undefined) {
+      return [hash_table[res], i];
+    } else {
+      hash_table[nums[i]] = i;
     }
   }
-  return array;
 };
 // @lc code=end
